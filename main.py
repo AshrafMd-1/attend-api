93ee3d3b24aeb89e50d98a045a9ff0c7be05f7b6
AshrafMd-1 <21951A6612@iare.ac.in>
2022-12-15 21:04:28 +0530

    Add rsa encryption to username and password

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 # import uvicorn as uvicorn
 from mod.login import login
 from mod.scrap import scrap_attend
+from auth.secure import decrypt
 from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 
 @app.get("/api/samvidha/attend/dis/v1")
+def attend(code=None):
+    if code is not None:
+        username, password = decrypt(code)
 
-# async def attend(
-#         payload: dict = Body(
-#             {
-#                 "username": "username",
-#                 "password": "password"
-#             }
-#         )):
-#     username = payload['username']
-#     password = payload['password']
-
-def attend(username=None,password=None):
-    
-    if username is not None and password is not None:
-        
         session, check = login(username, password)
 
         if check['status'] == '1':
@@ -29,21 +19,20 @@ def attend(username=None,password=None):
             return {
                 "message": "The attendance data is fetched successfully",
                 "data": df.to_dict(orient='records')
-                
+
             }
         else:
             return {
                 "message": "Invalid Credentials",
                 "data": []
-                
+
             }
     else:
         return {
             "message": "Please provide username and password",
             "data": []
-            
-        }
 
+        }
 
 #
 # if __name__ == "__main__":
